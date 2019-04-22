52eaeb0e50ad6b9cb3afdc2702d49019a7f30092
SonicCoding1180 <noahsturner06@gmail.com>
2019-04-22 11:47:13 -0400

    Update KAT.js

--- KAT.js
+++ KAT.js
@@ -1,2 +1,11 @@
 /**
+ * 
+ * KAT - A Khan Academy Terminal
+ * 
+ * Updates:
+ *  4/22/19 - First commit to the project.
+ * 
 **/
+
+// Initialize KATL.js
+Kit.init(Processing, document.getElementById("canvas-container"), "6627647894945792");
