123a00d13a3205678dae4102fece32f01df174ea
SonicCoding1180 <noahsturner06@gmail.com>
2019-04-22 12:14:20 -0400

    Update KAT.js

--- KAT.js
+++ KAT.js
@@ -8,4 +8,4 @@
 **/
 
 // Initialize KATL.js
-Kit.init(Processing, document.getElementById("canvas-container"), "kat-source-code/6627647894945792");
+Kit.init(Processing, document.getElementById("canvas-container"), 6627647894945792);
